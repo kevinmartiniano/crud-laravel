eb7c6335f532002b9f5030f9d091794560a635bd
kevinMartiniano <kevin.martiniano@hotmail.com>
2024-04-27 18:49:58 -0300

    feat: Creating contact factory to use in tests

---
+++ database/factories/ContactFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Contact>
+ */
+class ContactFactory extends Factory
+{
+    /**
+     * The current password being used by the factory.
+     */
+    protected static ?string $password;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'first_name' => fake()->name(),
+            'last_name' => fake()->lastName(),
+            'company' => fake()->company(),
+            'phone_number' => fake()->phoneNumber(),
+            'mobile_number' => fake()->phoneNumber(),
+            'email' => fake()->unique()->safeEmail(),
+            'birth_date' => fake()->date('Y-m-d', 'now'),
+        ];
+    }
+}
