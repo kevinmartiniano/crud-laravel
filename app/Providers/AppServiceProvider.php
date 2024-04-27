210f79e201052347dc2fc0b496d3d65ce63ea30d
kevinMartiniano <kevin.martiniano@hotmail.com>
2024-04-27 18:51:55 -0300

    feat: binding contract repository contact

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Repositories\ContactRepository;
+use App\Repositories\Contracts\ContactRepository as ContactRepositoryContract;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(
+            ContactRepositoryContract::class,
+            ContactRepository::class
+        );
     }
 
     /**
