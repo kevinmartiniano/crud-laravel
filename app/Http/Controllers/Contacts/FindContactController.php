a6462c8b5fc02fcce73bbb161502b8413a863b7a
kevinMartiniano <kevin.martiniano@hotmail.com>
2024-04-28 10:57:37 -0300

    chore: Hidrate error and implementation of tests

--- app/Http/Controllers/Contacts/FindContactController.php
+++ app/Http/Controllers/Contacts/FindContactController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers\Contacts;
 
 use App\Http\Controllers\Controller;
+use App\Models\Contact;
 use App\Services\ContactService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Log;
+use Throwable;
 
 class FindContactController extends Controller
 {
@@ -18,8 +21,31 @@ class FindContactController extends Controller
 
     public function __invoke(Request $request): JsonResponse
     {
-        return new JsonResponse([
-            'data' => $this->service->findContact($request->route('id'))->toArray()
-        ], Response::HTTP_OK);
+        try {
+            $contact = $this->service->findContact($request->route('id'));
+        } catch (Throwable $th) {
+            Log::error(
+                'error_find_contact_controller',
+                [
+                    'message' => $th->getMessage(),
+                    'stack_trace' => $th->getTraceAsString(),
+                    'file' => $th->getFile(),
+                    'line' => $th->getLine(),
+                ]
+            );
+
+            return new JsonResponse(null, Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        if ($contact instanceof Contact) {
+            return new JsonResponse(
+                [
+                    'data' => $contact->toArray(),
+                ],
+                Response::HTTP_OK
+            );
+        }
+
+        return new JsonResponse(null, Response::HTTP_NOT_FOUND);
     }
 }
--- tests/Feature/Http/Controllers/Contacts/FindContactControllerTest.php
+++ tests/Feature/Http/Controllers/Contacts/FindContactControllerTest.php
@@ -3,13 +3,19 @@
 namespace Tests\Feature\Controllers\Contacts;
 
 use App\Models\Contact;
+use App\Services\ContactService;
+use Exception;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Log;
+use Mockery;
 use Symfony\Component\HttpFoundation\Response;
 use Tests\TestCase;
 
 class FindContactControllerTest extends TestCase
 {
     use DatabaseMigrations;
+    use WithFaker;
 
     public function testShouldReceiveIdentifierAndReturnContact(): void
     {
@@ -38,5 +44,46 @@ class FindContactControllerTest extends TestCase
         
         $responseContact = data_get(json_decode($response->content(), true), 'data');
         $this->assertEquals($contact->toArray(), $responseContact);
+    }
+
+    public function testShouldReceiveIdentifierAndReturnNotFound(): void
+    {
+        $response = $this->get(sprintf('/contacts/%s', $this->faker->randomNumber(1)), [
+            'Content-Type' => 'application/json',
+        ]);
+
+        $response->assertStatus(Response::HTTP_NOT_FOUND);
+    }
+
+    public function testShouldReceiveIdentifierAndReturnException(): void
+    {
+        $id = $this->faker->randomNumber(1);
+        
+        $service = Mockery::mock(ContactService::class)->makePartial();
+        $this->app->instance(ContactService::class, $service);
+
+        $exception = new Exception();
+        $service->shouldReceive('findContact')
+            ->with($id)
+            ->andThrows($exception)
+            ->once();
+        
+        Log::shouldReceive('error')
+            ->with(
+                'error_find_contact_controller',
+                [
+                    'message' => $exception->getMessage(),
+                    'stack_trace' => $exception->getTraceAsString(),
+                    'file' => $exception->getFile(),
+                    'line' => $exception->getLine(),
+                ]
+            )
+            ->once();
+
+        $response = $this->get(sprintf('/contacts/%s', $id), [
+            'Content-Type' => 'application/json',
+        ]);
+
+        $response->assertStatus(Response::HTTP_INTERNAL_SERVER_ERROR);
     }
 }
