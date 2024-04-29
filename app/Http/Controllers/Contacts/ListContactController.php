9efe0ea0626f0ed1f9ce99edf7e73993819b7723
kevinMartiniano <kevin.martiniano@hotmail.com>
2024-04-29 16:37:00 -0300

    refactor: moving code to service.

--- app/Http/Controllers/Contacts/ListContactController.php
+++ app/Http/Controllers/Contacts/ListContactController.php
@@ -2,12 +2,10 @@
 
 namespace App\Http\Controllers\Contacts;
 
-use App\DTO\FilterListContactDTO;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\ListContactRequest;
 use App\Services\ContactService;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Log;
 use Throwable;
@@ -22,22 +20,8 @@ class ListContactController extends Controller
 
     public function __invoke(ListContactRequest $request): JsonResponse
     {
-        $filter = new FilterListContactDTO(
-            $request->get('id'),
-            $request->get('first_name'),
-            $request->get('last_name'),
-            $request->get('company'),
-            $request->get('phone_number'),
-            $request->get('mobile_number'),
-            $request->get('email')
-        );
-
-        $hidratedFilters = array_filter($filter->toArray(), function ($value) {
-            return !is_null($value) ? true : false;
-        }, ARRAY_FILTER_USE_BOTH);
-
         try {
-            $data = $this->service->findByArgs($hidratedFilters);
+            $data = $this->service->findByArgs($request->all());
         } catch (Throwable $th) {
             Log::error(
                 'error_list_contact_controller',
--- app/Services/ContactService.php
+++ app/Services/ContactService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use App\DTO\FilterListContactDTO;
 use App\Models\Contact;
 use App\Repositories\Contracts\ContactRepository;
 use Illuminate\Database\Eloquent\Collection;
@@ -21,6 +22,20 @@ class ContactService
 
     public function findByArgs(array $args = []): ?Collection
     {
-        return $this->contactRepository->where($args);
+        $filter = new FilterListContactDTO(
+            data_get($args, 'id'),
+            data_get($args, 'first_name'),
+            data_get($args, 'last_name'),
+            data_get($args, 'company'),
+            data_get($args, 'phone_number'),
+            data_get($args, 'mobile_number'),
+            data_get($args, 'email')
+        );
+
+        $hidratedFilters = array_filter($filter->toArray(), function ($value) {
+            return !is_null($value) ? true : false;
+        }, ARRAY_FILTER_USE_BOTH);
+
+        return $this->contactRepository->where($hidratedFilters);
     }
 }
